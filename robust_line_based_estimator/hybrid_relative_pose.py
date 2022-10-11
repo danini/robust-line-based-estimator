fc3537376891d9e319ea4f44becf12ecc91dcf09
danini <majti89@gmail.com>
2022-10-11 12:11:45 +0000

    Checking if any of the solvers are used.

--- robust_line_based_estimator/hybrid_relative_pose.py
+++ robust_line_based_estimator/hybrid_relative_pose.py
@@ -17,10 +17,13 @@ def run_hybrid_relative_pose(K1, K2, line_matches, vp_matches, junction_matches,
         solver_flags[3] = False
     if vp_matches[0].shape[1] < 1:
         solver_flags[2] = False
-
-    options = _estimators.HybridLORansacOptions()
-    options.data_type_weights_ = np.array([0.0, 0.0, 1.0])
-    options.squared_inlier_thresholds_ = np.array([0.0, 0.0, th_pixel])
-    res = _estimators.run_hybrid_relative_pose(K1, K2, line_matches, vp_matches, junction_matches, vp_labels, options, solver_flags)
-    return res[0][0], res[0][1]
+        
+    if any(solver_flags) == True:
+        options = _estimators.HybridLORansacOptions()
+        options.data_type_weights_ = np.array([0.0, 0.0, 1.0])
+        options.squared_inlier_thresholds_ = np.array([0.0, 0.0, th_pixel])
+        res = _estimators.run_hybrid_relative_pose(K1, K2, line_matches, vp_matches, junction_matches, vp_labels, options, solver_flags)
+        return res[0][0], res[0][1]
+    else:
+        return np.eye(3), np.zeros((3))
 
