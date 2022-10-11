df46bd29b0775743b5dea3c66bd424dcf645ecae
danini <majti89@gmail.com>
2022-10-11 11:54:31 +0000

    Turning off the VP solvers if there are not enough VPs

--- robust_line_based_estimator/hybrid_relative_pose.py
+++ robust_line_based_estimator/hybrid_relative_pose.py
@@ -4,6 +4,11 @@ sys.path.append("build/robust_line_based_estimator")
 import line_relative_pose_estimators as _estimators
 
 def run_hybrid_relative_pose(K1, K2, line_matches, vp_matches, junction_matches, vp_labels, th_pixel=1.0, solver_flags=[True, True, True, True]):
+    if vp_matches[0].shape[1] < 2:
+        solver_flags[3] = False
+    if vp_matches[0].shape[1] < 1:
+        solver_flags[2] = False
+        
     options = _estimators.HybridLORansacOptions()
     options.data_type_weights_ = np.array([0.0, 0.0, 1.0])
     options.squared_inlier_thresholds_ = np.array([0.0, 0.0, th_pixel])
