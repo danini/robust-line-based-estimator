0b2b73b9bdf51297b4c11e717cad18ea31a8845a
B1ueber2y <b1ueber2y@gmail.com>
2023-07-17 20:27:20 +0200

    delete tvec parameterization. not good empirically.

--- robust_line_based_estimator/refinement/ls_combined.cpp
+++ robust_line_based_estimator/refinement/ls_combined.cpp
@@ -96,17 +96,6 @@ void LeastSquares_Combined(const std::vector<JunctionMatch>& junction_matches,
         problem.SetManifold(qvec.data(), quaternion_manifold);
 #endif
     }
-    if (problem.HasParameterBlock(tvec.data())) {
-#ifdef CERES_PARAMETERIZATION_ENABLED
-        ceres::LocalParameterization* homo3d_parameterization = 
-            new ceres::HomogeneousVectorParameterization(3);
-        problem.SetParameterization(tvec.data(), homo3d_parameterization);
-#else
-        ceres::Manifold* homo3d_manifold = 
-            new ceres::SphereManifold<3>;
-        problem.SetManifold(tvec.data(), homo3d_manifold);
-#endif
-    }
 
     for (size_t vp_id = 0; vp_id < n_vps; ++vp_id) {
         // parameterize vp1
--- robust_line_based_estimator/refinement/ls_sampson.cpp
+++ robust_line_based_estimator/refinement/ls_sampson.cpp
@@ -39,17 +39,6 @@ void LeastSquares_Sampson(const std::vector<JunctionMatch>& junction_matches, st
         problem.SetManifold(qvec.data(), quaternion_manifold);
 #endif
     }
-    if (problem.HasParameterBlock(tvec.data())) {
-#ifdef CERES_PARAMETERIZATION_ENABLED
-        ceres::LocalParameterization* homo3d_parameterization = 
-            new ceres::HomogeneousVectorParameterization(3);
-        problem.SetParameterization(tvec.data(), homo3d_parameterization);
-#else
-        ceres::Manifold* homo3d_manifold = 
-            new ceres::SphereManifold<3>;
-        problem.SetManifold(tvec.data(), homo3d_manifold);
-#endif
-    }
 
     // Solve the optimization problem
     ceres::Solve(options, &problem, &summary);
